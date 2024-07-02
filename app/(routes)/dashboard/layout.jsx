9bdd17b85f8a9661203edafeec1ae867aab2b944
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-02 22:34:31 +0700

    adjust css of layout of dashboard

--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -3,7 +3,7 @@ import SideNav from "./_components/SideNav";
 const DashboardLayout = ({ children }) => {
   return (
     <div>
-      <div className="fixed md:w-64 hidden md:block bg-red-100">
+      <div className="fixed md:w-64 hidden md:block">
         <SideNav />
       </div>
       <div className="md:ml-64 bg-green-200">{children}</div>
