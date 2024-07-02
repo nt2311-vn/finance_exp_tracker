a33927d4a3d5ef588eff9173a77a44b57c8f88e6
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-03 01:19:15 +0700

    dbConfig

--- utils/dbConfig.js
+++ utils/dbConfig.js
@@ -1,11 +1,12 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
+import * as schema from "./schema.js";
 
 const client = createClient({
-	url: process.env.TURSO_DATABASE_URL,
-	authToken: process.env.TURSO_AUTH_TOKEN,
+	url: process.env.NEXT_PUBLIC_TURSO_DATABASE_URL,
+	authToken: process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN,
 });
 
-const db = drizzle(client);
+const db = drizzle(client, { schema });
 
 const result = await db.select().from(users).all();
