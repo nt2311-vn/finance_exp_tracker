a983fbf17ef76294fc7b525c2dd12980fc2cca6b
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-03 21:43:06 +0700

    update layout dashboard to dynamic naviage user to budget if they are not having one

--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -1,7 +1,32 @@
+"use client";
+import { db } from "@/utils/dbConfig";
 import DashboardHeader from "./_components/DashboardHeader";
 import SideNav from "./_components/SideNav";
+import { Budgets } from "@/utils/schema";
+import { eq } from "drizzle-orm";
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const DashboardLayout = ({ children }) => {
+  const { user } = useUser();
+  const router = useRouter();
+
+  const checkUserBudgets = async () => {
+    const result = await db
+      .select()
+      .from(Budgets)
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress));
+
+    if (result?.length === 0) {
+      router.replace("/dashboard/budget");
+    }
+  };
+
+  useEffect(() => {
+    user && checkUserBudgets();
+  }, [user]);
+
   return (
     <div>
       <div className="fixed md:w-64 hidden md:block">
