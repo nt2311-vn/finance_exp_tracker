a74bd262867463adeab7d21b13fc5a8680dec310
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-03 22:12:41 +0700

    CreateBudget Compoment

---
+++ app/(routes)/dashboard/budget/_components/CreateBudget.jsx
@@ -0,0 +1,10 @@
+const CreateBudget = () => {
+  return (
+    <div className="text-slate-300 p-10 rounded-md items-center flex flex-col border-2 border-dashed">
+      <h2 className="text-3xl">+</h2>
+      <h2>Create New Budget</h2>
+    </div>
+  );
+};
+
+export default CreateBudget;
