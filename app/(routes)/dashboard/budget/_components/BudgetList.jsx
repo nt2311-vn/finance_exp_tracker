0152b022c2ecaab8901052d769412484ddca3e2a
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-04 23:12:37 +0700

    adjust css of BUdgetList

--- app/(routes)/dashboard/budget/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budget/_components/BudgetList.jsx
@@ -2,8 +2,10 @@ import CreateBudget from "./CreateBudget";
 
 const BudgetList = () => {
   return (
-    <div>
-      <CreateBudget />
+    <div className="mt-7">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <CreateBudget />
+      </div>
     </div>
   );
 };
