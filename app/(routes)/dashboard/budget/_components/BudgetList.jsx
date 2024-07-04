e259a3b914554bf3fbe5358ebc29eb34d10af70b
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-05 01:58:49 +0700

    render the budget item and using the server function to get budget list

--- app/(routes)/dashboard/budget/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budget/_components/BudgetList.jsx
@@ -1,10 +1,16 @@
+import BudgetItem from "./BudgetItem";
 import CreateBudget from "./CreateBudget";
+import getBudgetList from "@/app/(actions)/getBudgetList";
 
-const BudgetList = () => {
+const BudgetList = async () => {
+  const budgets = await getBudgetList();
   return (
     <div className="mt-7">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         <CreateBudget />
+        {budgets.map((budget, index) => (
+          <BudgetItem budget={budget} key={index} />
+        ))}
       </div>
     </div>
   );
