6e7ec1d54cd5b443336471f9fe9db99ca9fe55b4
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-05 21:41:30 +0700

    Complete BudgetItem component

--- app/(routes)/dashboard/budget/_components/BudgetItem.jsx
+++ app/(routes)/dashboard/budget/_components/BudgetItem.jsx
@@ -1,20 +1,20 @@
 const BudgetItem = ({ budget }) => {
   return (
-    <div className="p-5 border rounded-lg">
+    <div className="p-5 border rounded-lg hover:shadow-lg hover:bg-slate-100 cursor-pointer">
       <div className="flex gap-2 items-center justify-between">
         <div className="flex gap-2 items-center">
           <h2 className="text-2xl p-3 px-4 bg-slate-200 rounded-full">
             {budget?.icon}
           </h2>
           <div>
-            <h2>{budget?.name}</h2>
-            <h2>{budget?.totalItem} Item</h2>
+            <h2 className="font-bold">{budget?.name}</h2>
+            <h2 className="text-sm text-gray-500">{budget?.totalItem} Item</h2>
           </div>
         </div>
         <h2 className="font-bold text-primary text-lg">${budget?.amount}</h2>
       </div>
       <div className="mt-5">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mb-3">
           <h2 className="text-md text-slate-400 font-medium">
             ${budget?.totalSpend ?? 0} Spent
           </h2>
