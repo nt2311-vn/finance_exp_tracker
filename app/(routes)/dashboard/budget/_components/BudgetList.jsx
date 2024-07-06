2dd5ae8ef77f22d4babe2d617561d5a4d22de223
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-06 14:48:03 +0700

    loadon client render

--- app/(routes)/dashboard/budget/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budget/_components/BudgetList.jsx
@@ -1,41 +1,15 @@
-"use client";
-import BudgetItem from "./BudgetItem";
-import CreateBudget from "./CreateBudget";
+import { Suspense } from "react";
+import BudgetListClient from "./BudgetListClient";
+import BudgetListLoading from "./BudgetListLoading";
 import getBudgetList from "@/app/(actions)/getBudgetList";
-import { useEffect, useState } from "react";
 
 const BudgetList = async () => {
-  const [budgets, setBudgets] = useState([]);
-
-  useEffect(() => {
-    const fetchBudgets = async () => {
-      try {
-        const budgets = await getBudgetList();
-        setBudgets(budgets);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchBudgets();
-  });
+  const budgets = await getBudgetList();
 
   return (
-    <div className="mt-7">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        <CreateBudget />
-        {budgets.length > 0
-          ? budgets.map((budget, index) => (
-              <BudgetItem budget={budget} key={index} />
-            ))
-          : [1, 2, 3, 4, 5].map((item, index) => (
-              <div
-                key={index}
-                className="w-full bg-slate-200 rounded-lg h-[180px] animate-pulse"
-              ></div>
-            ))}
-      </div>
-    </div>
+    <Suspense fallback={<BudgetListLoading />}>
+      <BudgetListClient budgets={budgets} />
+    </Suspense>
   );
 };
 
