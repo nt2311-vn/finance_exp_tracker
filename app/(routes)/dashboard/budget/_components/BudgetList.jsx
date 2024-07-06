d32f136c2930050e3e45f34b10bb706edbd39996
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-06 10:04:02 +0700

    use client action

--- app/(routes)/dashboard/budget/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budget/_components/BudgetList.jsx
@@ -1,16 +1,39 @@
+"use client";
 import BudgetItem from "./BudgetItem";
 import CreateBudget from "./CreateBudget";
 import getBudgetList from "@/app/(actions)/getBudgetList";
+import { useEffect, useState } from "react";
 
 const BudgetList = async () => {
-  const budgets = await getBudgetList();
+  const [budgets, setBudgets] = useState([]);
+
+  useEffect(() => {
+    const fetchBudgets = async () => {
+      try {
+        const budgets = await getBudgetList();
+        setBudgets(budgets);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchBudgets();
+  });
+
   return (
     <div className="mt-7">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         <CreateBudget />
-        {budgets.map((budget, index) => (
-          <BudgetItem budget={budget} key={index} />
-        ))}
+        {budgets.length > 0
+          ? budgets.map((budget, index) => (
+              <BudgetItem budget={budget} key={index} />
+            ))
+          : [1, 2, 3, 4, 5].map((item, index) => (
+              <div
+                key={index}
+                className="w-full bg-slate-200 rounded-lg h-[180px] animate-pulse"
+              ></div>
+            ))}
       </div>
     </div>
   );
